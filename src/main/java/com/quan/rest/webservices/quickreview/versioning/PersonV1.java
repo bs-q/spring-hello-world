e3d75fb748efc0255187be21616598e1a5ea0bf4
9Queen <18110041@student.hcmute.edu.vn>
2020-12-29 23:02:27 +0700

    filtering and versioning

---
+++ src/main/java/com/quan/rest/webservices/quickreview/versioning/PersonV1.java
@@ -0,0 +1,21 @@
+package com.quan.rest.webservices.quickreview.versioning;
+
+public class PersonV1 {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public PersonV1() {
+    }
+
+    public PersonV1(String name) {
+        this.name = name;
+    }
+
+}
---
+++ src/main/java/com/quan/rest/webservices/quickreview/versioning/PersonV2.java
@@ -0,0 +1,21 @@
+package com.quan.rest.webservices.quickreview.versioning;
+
+public class PersonV2 {
+    private Name name;
+
+    public Name getName() {
+        return name;
+    }
+
+    public void setName(Name name) {
+        this.name = name;
+    }
+
+    public PersonV2() {
+    }
+
+    public PersonV2(Name name) {
+        this.name = name;
+    }
+
+}
